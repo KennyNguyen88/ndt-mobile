3964c216fc6f7b093e96eef47ad098fe723931d0
KennyNguyen88 <giatrangrua@gmail.com>
2016-09-06 10:57:04 +0700

    Managing Database with Content Provider Adding CRUD methods to Content Provider

--- LyndaNoteTaking/app/src/main/java/com/example/trung/lyndanotetaking/MainActivity.java
+++ LyndaNoteTaking/app/src/main/java/com/example/trung/lyndanotetaking/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.trung.lyndanotetaking;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,8 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DBOpenHelper helper = new DBOpenHelper(this);
-        SQLiteDatabase database = helper.getWritableDatabase();
+        insertNote("New Note");
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -31,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT,noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        Log.d("Main Activity", "Inserted note " + noteUri.getLastPathSegment());
     }
 
     @Override
