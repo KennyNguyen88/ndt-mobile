db9c327d21ef8a593deef9bf07a20f70ad522aa5
KennyNguyen88 <giatrangrua@gmail.com>
2016-09-06 23:37:24 +0700

    Updating an existing note

--- LyndaNoteTaking/app/src/main/java/com/example/trung/lyndanotetaking/EditorActivity.java
+++ LyndaNoteTaking/app/src/main/java/com/example/trung/lyndanotetaking/EditorActivity.java
@@ -2,16 +2,20 @@ package com.example.trung.lyndanotetaking;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditorActivity extends AppCompatActivity {
 
     private String action;
     private EditText editor;
+    private String noteFilter;
+    private String oldText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +29,13 @@ public class EditorActivity extends AppCompatActivity {
         if (uri == null){
             action = Intent.ACTION_INSERT;
             setTitle(getString(R.string.new_note));
+        }else{
+            action = Intent.ACTION_EDIT;
+            noteFilter = DBOpenHelper.NOTE_ID + "=" + uri.getLastPathSegment();
+
+            Cursor cursor = getContentResolver().query(uri, DBOpenHelper.ALL_COLUMNS, noteFilter,null,null);
+            oldText = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_TEXT));
+            editor.requestFocus();
         }
     }
 
@@ -39,8 +50,30 @@ public class EditorActivity extends AppCompatActivity {
                 {
                     insertNote(newText);
                 }
+                break;
+            case Intent.ACTION_EDIT:
+                if (newText.length() == 0){
+                    deleteNote();
+                }else if (oldText.equals(newText)){
+                    setResult(RESULT_CANCELED);
+                }
+                else{
+                    updateNote(newText);
+                }
         }
         finish();
+    }
+
+    private void updateNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT,noteText);
+        getContentResolver().update(NotesProvider.CONTENT_URI,values, noteFilter,null);
+        Toast.makeText(this, R.string.note_updated, Toast.LENGTH_SHORT).show();
+        setResult(RESULT_OK);
+    }
+
+    private void deleteNote() {
+        
     }
 
     private void insertNote(String noteText) {
